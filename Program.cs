6eb36ffded5b49d83d6479f26c3ed9aea599b691
Maria <masya.2506@mail.ru>
2022-09-16 16:59:21 +0300

    Print_Array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+
+Console.Write("Введите массив строк через пробел:");
+string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            //.Select(x => x)
+                            .ToArray();
+
+PrintArray(array);
+
+void PrintArray(string[] inArray) // печать матрицы
+{
+    Console.Write("[");
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Console.Write($"{inArray[i]} ");
+    }
+    Console.Write("]");
+}
